8e1243491b9cae75a61a2f3c85e82018b4da862c
Alberto De Marchi <aldmarchi@gmail.com>
2026-08-07 12:14:17 +0200

    formatting

--- main_bb.m
+++ main_bb.m
@@ -3,70 +3,71 @@ close all
 clc
 
 %% flow map parameters
-T_s=.01;
-g=9.8;
-lambda=.7;
-A_f=[1 T_s
-    0 1];
-B_f=[0
-    0];
-C_f=[-T_s^2*g
-    -T_s*g];
+T_s = 0.01;
+g = 9.8;
+lambda = 0.7;
+A_f = [1, T_s; 0, 1];
+B_f = [0; 0];
+C_f = [-T_s^2*g; -T_s*g];
+
 %% jump map parameters
-A_g=[1 -T_s
-    0 -lambda];
-B_g=[0
-    1];
-C_g=[0
-    0];
+A_g = [1, -T_s; 0, -lambda];
+B_g = [0; 1];
+C_g = [0; 0];
+
 %% jump and flow set  parameters
-h=[-1, 0];
-%    0 -1];
-sigma=0;
+h = [-1, 0];
+sigma = 0;
+
 %% feasiblity set
-x_1max=10;
-x_1min=-10;
-x_2max=10;
-x_2min=-10;
-u_max=.01;
-u_min=-.01;
-xmax=[x_1max
-    x_2max];
-xmin=[x_1min
-    x_2min];
+x_1max = 10;
+x_1min = -10;
+x_2max = 10;
+x_2min = -10;
+u_max = 0.01;
+u_min = -0.01;
+xmax=[x_1max; x_2max];
+xmin=[x_1min; x_2min];
+
 %% finding max and min of flow map
 M11=x_1max;
 M12=x_2max;
 m11=x_1min;
 m12=x_2min;
-m1=[m11
-    m12];
-M1=[M11
-    M12];
+m1=[m11; m12];
+M1=[M11; M12];
+
 %% finding max and min of jump map
 M2=u_max;
 m2=u_min;
+
 %% max and min on h(x)
-cc8=[h(1);h(2)]>=0;
-M3=h(1)*(cc8(1)*x_1max+(1-cc8(1))*x_1min)+h(2)*(cc8(2)*x_2max+(1-cc8(2))*x_2min);
-cc9=[h(1);h(2)]<=0;
-m3=h(1)*(cc9(1)*x_1max+(1-cc9(1))*x_1min)+h(2)*(cc9(2)*x_2max+(1-cc9(2))*x_2min);
+cc8 = [h(1);h(2)]>=0;
+M3  = h(1)*(cc8(1)*x_1max+(1-cc8(1))*x_1min)+h(2)*(cc8(2)*x_2max+(1-cc8(2))*x_2min);
+cc9 = [h(1);h(2)]<=0;
+m3  = h(1)*(cc9(1)*x_1max+(1-cc9(1))*x_1min)+h(2)*(cc9(2)*x_2max+(1-cc9(2))*x_2min);
+
 %% calculating AA for x(\ell)=AA*[z(\ell-1),u_f(\ell-1),u(\ell-1)]
 AA=AA(A_f,A_g,B_f,B_g,C_f,C_g);
+
 %% calculating A for F1*X<b
 A=F1(AA,A_g,h,M11,M12,M2,M3,m11,m12,m2,m3,sigma);
+
 %% cost function parameters
-Q_c=2e-1*eye(2);
-Q_d=2e-1*eye(2);
-P=1e-1*eye(2);
-R_c=1e-2;
-R_d=1e-2;
+Q_c = 2e-1*eye(2);
+Q_d = 2e-1*eye(2);
+P   = 1e-1*eye(2);
+R_c = 1e-2;
+R_d = 1e-2;
+
 %% initial value
-x0=zeros(20,1);
-x1=2;
-x2=0;
-T_f=349; % simulation time
-T_p=1; % control horizon
+x1 = 2;
+x2 = 0;
+T_f = 349; % simulation time
+T_p = 1; % control horizon
+N = 4;
+optx0 = zeros(5*N,1);
+
 x_1 = nan(1,T_f+1);
 x_2 = nan(1,T_f+1);
 x_1(1) = x1;
@@ -75,22 +76,21 @@ u_f = nan(1,T_f);
 u_ = nan(1,T_f);
 
 % binary variables
-ivar=[4,9,14,19];
+ivar = 4:5:(5*N);
 
 % solver configuration
 options = [];
 
 for i=1:T_f
     %% b for next steps that x(i) is written as sumation of z_1 and z_2 so we dont need them in b
-    x=[x1
-        x2];
+    x = [x1; x2];
 
-    [S1,S2]=costfunction(AA,Q_c,A_g,Q_d,R_d,R_c,P,x);
-    
-    bb=b(A_g,x,x1,x2,m2,M11,M12,M2,h,sigma,M3,xmax,xmin,m11,m12,u_max,u_min);
+    % optvec = [z1,rho1,u1, z2,rho2,u2, ..., zN,rhoN,uN]
+    [S1,S2] = costfunction(AA,Q_c,A_g,Q_d,R_d,R_c,P,x);
+    bb = b(A_g,x,x1,x2,m2,M11,M12,M2,h,sigma,M3,xmax,xmin,m11,m12,u_max,u_min);
     
     % solver call
-    optsol = miqp(S1,S2,A,bb,[],[],ivar,[],[],x0,options);
+    optsol = miqp(S1,S2,A,bb,[],[],ivar,[],[],optx0,options);
 
     x_11=AA*optsol(1:5)+A_g*x+C_g;
     x1=x_11(1);
@@ -101,7 +101,7 @@ for i=1:T_f
     u_(i) = optsol(5);
 
     % warmstart
-    x0 = optsol;
+    optx0 = optsol;
 end
 
 %% plotting
